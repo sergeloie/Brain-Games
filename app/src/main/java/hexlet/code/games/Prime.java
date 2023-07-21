31833bb84c5d6076bab31ad5bdd58cda2bc8f9d8
serge.loie.voxys <serge.loie@yandex.ru>
2023-07-21 15:58:21 +0500

    обработка для чисел меньше 2

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -27,6 +27,9 @@ public class Prime {
     }
 
     private static boolean isPrime(int x) {
+        if (x < 2) {
+            return false;
+        }
         for (int i = 2; i <= Math.sqrt(x); i++) {
             if (x % i == 0) {
                 return false;
